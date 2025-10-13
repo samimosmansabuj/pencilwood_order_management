e142e784aaea221ccccc3fe786f3692dfc5079e3
samimosmansabuj <samim.o.sabuj01@gmail.com>
2025-10-14 03:32:57 +0600

    add-new-order-2 page product list, price and calculation ui!!!

--- order/views2.py
+++ order/views2.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render, redirect
 from .models import Product
+from .forms import OrderCustomerForm, OrderForm
 
 def add_new_order2(request):
     products = Product.objects.all()
-    return render(request, "add_new_order2.html", {'products': products})
+    order_customer_form = OrderCustomerForm()
+    order_form = OrderForm()
+    return render(request, "add_new_order2.html", {'products': products, 'order_customer_form': order_customer_form, 'order_form': order_form})
 
 
