4db54dc73e5b316447474658b03b0b66f3fa0333
samimosmansabuj <samim.o.sabuj01@gmail.com>
2025-10-18 22:50:02 +0600

    media file error solved!

--- pencilwood_order_management/urls.py
+++ pencilwood_order_management/urls.py
@@ -1,7 +1,8 @@
 from django.conf import settings
-from django.conf.urls.static import static
+from django.views.static import serve as static_serve
+import os
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 
 
 
@@ -14,9 +15,15 @@ urlpatterns = [
     path('', include('order.urls')),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+
+SERVE_MEDIA = os.getenv("SERVE_MEDIA", "False").strip().lower() in ("true","1","yes")
+
+if SERVE_MEDIA:
+    urlpatterns += [
+        re_path(r'^media/(?P<path>.*)$', static_serve, {'document_root': settings.MEDIA_ROOT}),
+        re_path(r'^static/(?P<path>.*)$', static_serve, {'document_root': settings.STATIC_ROOT}),
+    ]
 
 
 
